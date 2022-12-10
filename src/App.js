75dcec1a6aafaa484e76aa3f4aa56e0ffa7888e1
OlegKavun <kaunolegs@gmail.com>
2022-12-10 15:59:29 +0200

    fixed tg issue

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import './App.css';
 import { useTelegram } from './hooks/useTelegram';
 
 function App() {
+  const {onToggleButton, tg} = useTelegram();
+
   useEffect(() => {
     tg.ready();
   }, [])
-
-  const {onToggleButton, tg} = useTelegram();
   
   return (
     <div className="App">
-      Work
       <button onClick={onToggleButton}>Toggle</button>
     </div>
   );
